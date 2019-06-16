7e548a70cfecfcb5712286e01b3584f54392be79
bartosz.magrys <it165@g2a.com>
2019-06-16 13:37:53 +0200

    #4 Prepared build configuration for Orders micro app. Implemented logic for attaching custom element in Envelope app.

--- micro-frontend-envelope/src/app/envelope/component/micro-app/micro-app.component.ts
+++ micro-frontend-envelope/src/app/envelope/component/micro-app/micro-app.component.ts
@@ -1,8 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-envelope-micro-app',
-  template: `<div id="micro-app-web-component">There should render micro-app!</div>`
+  template: `
+    <div id="micro-app-web-component">There should render micro-app!</div>`
 })
-export class MicroAppComponent {
+export class MicroAppComponent implements OnInit {
+  private CUSTOM_ELEMENT_CONTAINER_ID = "micro-app-web-component";
+  private appSelector = 'micro-frontend-app-orders-root';
+  private appUrlPrefix = 'http://localhost:8201/micro-frontend-app-orders';
+
+  constructor() {
+  }
+
+  public ngOnInit() {
+    const content = document.getElementById(this.CUSTOM_ELEMENT_CONTAINER_ID);
+    if (!this.isRegistered(this.appSelector)) {
+      console.debug(`Loading Web Component ${this.appSelector} from ${this.appUrlPrefix}`);
+      this.loadScripts(content, this.appUrlPrefix);
+    } else {
+      console.debug('Web Component {} already loaded', this.appSelector);
+    }
+
+    const element: HTMLElement = document.createElement(this.appSelector);
+    content.innerHTML = '';
+    content.appendChild(element);
+  }
+
+  private loadScripts(containerElement: HTMLElement, url: string) {
+    for (const standard of ['es5', 'es2015']) {
+      const script = document.createElement('script');
+      script.src = `${url}/main-${standard}.js`;
+      if ('es5' === standard) {
+        script.noModule = true;
+      } else if ('es2015' === standard) {
+        script.type = "module";
+      }
+      containerElement.appendChild(script);
+      script.onerror = () => {
+        throw new Error(`Error occurred when loading ${url}`);
+      };
+    }
+  }
+
+  private isRegistered(name) {
+    return document.createElement(name).constructor !== HTMLElement;
+  }
 }
