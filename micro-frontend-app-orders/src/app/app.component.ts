5c51cf41cf83b75d08f54b011734aafeff36532d
bartosz.magrys <it165@g2a.com>
2019-06-16 12:19:14 +0200

    #3 Redefine application to be a registered custom element

--- micro-frontend-app-orders/src/app/app.component.ts
+++ micro-frontend-app-orders/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'micro-frontend-app-orders-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- micro-frontend-app-orders/src/app/app.module.ts
+++ micro-frontend-app-orders/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {DoBootstrap, Injector, NgModule} from '@angular/core';
+import {createCustomElement} from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,15 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private readonly injector: Injector) {
+  }
+
+  public ngDoBootstrap() {
+    const myElement = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('micro-frontend-app-orders-root', myElement);
+  }
+}
