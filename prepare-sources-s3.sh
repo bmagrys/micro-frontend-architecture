9c07b0259c706a711bc1bad4f7b22980da6a3086
Bartosz Magryś <bartoszmagrys7@gmail.com>
2019-06-20 22:10:54 +0200

    Hotfix for AWS upload script

--- prepare-sources-s3.sh
+++ prepare-sources-s3.sh
@@ -4,4 +4,5 @@ npm run --prefix micro-frontend-app-orders build:aws
 rm -rf ./static/
 cp -R micro-frontend-envelope/dist/ ./static/
 cp -R micro-frontend-app-orders/dist/micro-frontend-app-orders ./static/
+aws s3 rm s3://$1 --recursive
 aws s3 sync ./static/ s3://$1/
